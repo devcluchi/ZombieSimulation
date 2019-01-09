211885b9a973697bbd3dc13e1980ea63e81a0d63
devcluchi <luis.boehme@gmx.net>
2019-01-09 22:38:57 +0100

    Notizen fertig gestellt

--- src/model/screens/NotizenScreen.java
+++ src/model/screens/NotizenScreen.java
@@ -5,12 +5,15 @@ import model.framework.GraphicalObject;
 import view.framework.DrawTool;
 
 import java.awt.image.BufferedImage;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class NotizenScreen extends GraphicalObject {
 
     private boolean visible;
     private BufferedImage background;
     private MainScreen mainScreen;
+    private String tiere, toteMenschen, essen, medikamente, waffen;
 
     public NotizenScreen(MainScreen mainScreen) {
         this.mainScreen = mainScreen;
@@ -22,7 +25,15 @@ public class NotizenScreen extends GraphicalObject {
     public void draw(DrawTool drawTool) {
         if(visible){
             //drawTool.drawImage(background,200,100);
-            drawTool.drawFilledRectangle(150,100,200,200);
+            drawTool.setCurrentColor(255,55,55,255);
+            drawTool.drawFilledRectangle(150,100,500,500);
+            drawTool.setCurrentColor(255,255,255,255);
+            drawTool.drawText(300,150,"Tiere: "+tiere);
+            drawTool.drawText(300,250,"getötete Menschen: "+toteMenschen);
+            drawTool.drawText(300,350,"Essen: "+essen);
+            drawTool.drawText(300,450,"Medikamente: "+medikamente);
+            drawTool.drawText(300,550,"Waffen: "+waffen);
+
         }
     }
 
@@ -32,12 +43,30 @@ public class NotizenScreen extends GraphicalObject {
     }
 
     public void updateInformations(){
-        //mainScreen.getLogic().getTableManager().getStmt().executeQuery();
+        ResultSet resultSet = null;
+        try {
+            resultSet = mainScreen.getLogic().getTableManager().getStmt().executeQuery("SELECT Bestand FROM Zom_Nutztiere WHERE nID = 1;");
+            resultSet.next();
+            tiere = resultSet.getString("Bestand");
+            resultSet = mainScreen.getLogic().getTableManager().getStmt().executeQuery("SELECT COUNT(infiziert) FROM Zom_Zombie;");
+            resultSet.next();
+            toteMenschen = resultSet.getString(1);
+            resultSet = mainScreen.getLogic().getTableManager().getStmt().executeQuery("SELECT Vorrat FROM Zom_Essen WHERE eID = 1;");
+            resultSet.next();
+            essen = resultSet.getString("Vorrat");
+            resultSet = mainScreen.getLogic().getTableManager().getStmt().executeQuery("SELECT Vorrat FROM Zom_Medikament WHERE mID = 1;");
+            resultSet.next();
+            medikamente = resultSet.getString("Vorrat");
+            waffen = Integer.toString(mainScreen.getLogic().getWaffenVorrat());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void setVisible(boolean visible) {
         this.visible = visible;
+        updateInformations();
     }
 
     public boolean isVisible() {
